01830266b3d79bff73ce404c4bf675f80ddc1a4e
tmknom <tmk.nom@gmail.com>
2020-09-14 07:33:43 +0900

    Add example for 08

---
+++ 08/01_aws_budgets_budget.tf
@@ -0,0 +1,16 @@
+resource "aws_budgets_budget" "monthly_cost" {
+  name              = "MonthlyCost"
+  budget_type       = "COST"
+  limit_amount      = "10.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2020-01-01_00:00"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["test@example.com"]
+  }
+}
