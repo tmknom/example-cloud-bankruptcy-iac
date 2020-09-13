702a0ecff5311c15188ffc935bfb58f7644fd111
tmknom <tmk.nom@gmail.com>
2020-09-14 08:30:28 +0900

    Add example for 31

---
+++ 31/01_aws_iam_policy_document.tf
@@ -0,0 +1,18 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
